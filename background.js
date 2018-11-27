8776a5defb2ff878c3015e67c03d8ba081a3bd58
Liam DeVoe <orionldevoe@gmail.com>
2018-11-26 22:04:45 -0500

    color icon when on relevant pages

--- background.js
+++ background.js
@@ -2,6 +2,8 @@ chrome.tabs.onUpdated.addListener(function(tabId, details, tab){
 	if(details.status != "complete" || !tab.url.includes("mcps")){
 		return
 	}
+
+	chrome.pageAction.show(tabId);
 	chrome.tabs.executeScript({file:"lib/jquery-3.3.1.min.js"});
     chrome.tabs.executeScript({file:"contentScript.js"});
 });
